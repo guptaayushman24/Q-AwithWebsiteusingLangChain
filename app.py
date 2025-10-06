4928a95011e3b3a39bb588ce69f24a2ba7d144ce
Ayushman Gupta <AyushmanGupta@bdo.in>
2025-10-06 17:35:04 +0530

    ipynb and py code implemented

---
+++ app.py
@@ -0,0 +1,64 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import WebBaseLoader
+load_dotenv()
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain.schema import Document
+from langchain_community.vectorstores import FAISS
+from langchain_openai import ChatOpenAI
+from langchain.chains import RetrievalQA
+from fastapi import FastAPI
+from langserve import add_routes
+os.environ['OPEN_API_KEY']=os.getenv("OPENAI_API_KEY")
+## LangSmith Tracking
+os.environ['LANGCHAIN_API_KEY']=os.getenv("LANGCHAIN_API_KEY")
+os.environ['LANGCHAIN_TRACKING_V2']="true"
+
+os.environ['LANGCHAIN_PROJECT']=os.getenv("LANGCHAIN_PROJECT")
+
+try :
+
+    loader = WebBaseLoader('https://en.wikipedia.org/wiki/OpenAI')
+    docs = loader.load()
+    # Splitting Data
+    from langchain_text_splitters import RecursiveCharacterTextSplitter
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=550,chunk_overlap=150)
+    documents = text_splitter.split_documents(docs)
+
+    documents_page_content = []
+    for i in documents :
+        documents_page_content.append(i.page_content)
+
+    clean_docs = [item.replace("\n", "").strip() for item in documents_page_content]
+    embeddings = OpenAIEmbeddings()
+    embeddings_documents = embeddings.embed_documents(clean_docs)
+    langchain_document  = [Document(page_content=text) for text in clean_docs]
+    vectorstoredb = FAISS.from_documents(langchain_document,embeddings)
+    llm = ChatOpenAI(model="gpt-4o") # Calling the specific model
+
+    retriver = vectorstoredb.as_retriever(search_kwargs={"k":3})
+    retriever = vectorstoredb.as_retriever(search_kwargs={"k": 3})
+    # Build Chain
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever
+    )
+
+    app = FastAPI(title="LangChain Server",
+                  version="1.0",
+                  description="Question and Answer about the Open AI History"
+                  )
+    
+    add_routes(
+        app,
+        qa_chain,
+        path='/chain'
+    )
+
+    if __name__=="__main__" :
+        import uvicorn
+        uvicorn.run(app,host="127.0.0.1",port=8000)
+except RuntimeError as e:
+    print(e)
